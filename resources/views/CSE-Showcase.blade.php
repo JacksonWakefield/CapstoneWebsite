78b9251933e7f0971ab7b01bbbbd7a6a44694095
Jackson Wakefield <jacksonwakefield01@gmail.com>
2024-03-18 13:02:03 -0700

    Pushed auto-imbed for info passed through JASON

--- resources/views/CSE-Showcase.blade.php
+++ resources/views/CSE-Showcase.blade.php
@@ -1,32 +1,35 @@
 <head>
     <link rel="stylesheet" href="<?php echo asset('/css/cse-showcase.css')?>" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Oswald:wght@200..700&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
 </head>
 
 
 <body>
 <!-- SHOWCASE COMPONENT !-->
-    <?php foreach(array(1,2,3,4,5,6,7,8,9) as &$value): ?>
+    <div class="TopBanner"></div>
+    <?php $i = 0;?>
+    <?php foreach($listings as &$listing): ++$i ?>
     <section class="showcase-single">
-        <div class="showcase-single-wrapper">
+        <div class="showcase-single-wrapper <?=$i % 2 === 0 ? 'flex-right' : 'flex-left'?>">
             <div class="showcase-single-yt-wrapper individual-wrapper">
                 <!-- <a href="https://www.youtube.com/watch?v=ZAQwg4BtbYo"></a> *TODO: replace with youtube wrapper* !-->
-                <img id="yt-link" src="<?php echo asset('/images/sample-image-yt.jpg')?>"></img>
+                <iframe id="yt_frame" src="{{$listing['YT_Link']}}"></iframe>
                 <p class="showcase-single-yt-p">
-                    Team Members are:
+                    Team Members are:   
                 </p>
                 <p class="showcase-single-yt-p">
-                    Jackson Wakefield, Jacob Licko, Jon Doe, Princess Diana
+                    {{$listing['Team Members']}}
                 </p>
             </div>
             <div class="showcase-single-description-wrapper individual-wrapper">
-                <h2 class="showcase-single-description-title">
-                    CSE 486 Sample Project
+                <h2 class="showcase-single-description-title <?=$i % 2 === 0 ? 'align-right' : 'align-left'?>">
+                    {{$listing['Team Name']}}
                 </h2>
                 <p class="showcase-single-description-p">
-                    This is the sample text for a CSE 486 project. In this project nothing actually happens.
-                    In fact, this project is in a limital space in between being - meaning that it constantly
-                    fluctuates between existing and not existing - an eternal torture of sorts. Like a vivid
-                    dream that lasts forever. 
+                    {{$listing['Description']}}
                 </p>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,58 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('CSE-Showcase');
+    return view('CSE-Showcase', [ // PLACEHOLDER
+        'listings' => [
+            [   
+                'Team Name' => 'Sample Team',
+                'YT_Link'=> 'https://www.youtube.com/embed/MYyJ4PuL4pY?si=vch-Vmz9gT0TiaUI',
+                'Team Members'=> 'Jackson Wakefield, Jacob Licko',
+                'Description'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            ],
+            [   
+                'Team Name' => 'Another Team',
+                'YT_Link'=> 'https://www.youtube.com/embed/tgbNymZ7vqY',
+                'Team Members'=> 'Hunter M, Kate Middleton (MIA)',
+                'Description'=> 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+            ],
+            [   
+                'Team Name' => 'Sample Team',
+                'YT_Link'=> 'https://www.youtube.com/embed/MYyJ4PuL4pY?si=vch-Vmz9gT0TiaUI',
+                'Team Members'=> 'Jackson Wakefield, Jacob Licko',
+                'Description'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            ],
+            [   
+                'Team Name' => 'Another Team',
+                'YT_Link'=> 'https://www.youtube.com/embed/tgbNymZ7vqY',
+                'Team Members'=> 'Hunter M, Kate Middleton (MIA)',
+                'Description'=> 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+            ],
+            [   
+                'Team Name' => 'Sample Team',
+                'YT_Link'=> 'https://www.youtube.com/embed/MYyJ4PuL4pY?si=vch-Vmz9gT0TiaUI',
+                'Team Members'=> 'Jackson Wakefield, Jacob Licko',
+                'Description'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            ],
+            [   
+                'Team Name' => 'Another Team',
+                'YT_Link'=> 'https://www.youtube.com/embed/tgbNymZ7vqY',
+                'Team Members'=> 'Hunter M, Kate Middleton (MIA)',
+                'Description'=> 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+            ],
+            [   
+                'Team Name' => 'Sample Team',
+                'YT_Link'=> 'https://www.youtube.com/embed/MYyJ4PuL4pY?si=vch-Vmz9gT0TiaUI',
+                'Team Members'=> 'Jackson Wakefield, Jacob Licko',
+                'Description'=> 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            ],
+            [   
+                'Team Name' => 'Another Team',
+                'YT_Link'=> 'https://www.youtube.com/embed/tgbNymZ7vqY',
+                'Team Members'=> 'Hunter M, Kate Middleton (MIA)',
+                'Description'=> 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.'
+            ]
+        ]
+    ]); 
 });
 
 Route::get('/seminars', function () {
